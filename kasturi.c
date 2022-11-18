66630dbe6cae1d6682a20582894fca22bc284884
Kasturi2003 <kasturi.marvel@gmail.com>
2022-11-18 20:11:11 +0530

    uploaded by kasturi

---
+++ kasturi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(i==1 || i==5)
+                {
+                    if(j==1 || j==5)
+                        printf("+");
+                    else
+                        printf("-");
+                }
+            else
+            {
+                if(j==1 || j==5)
+                    printf("|");
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
